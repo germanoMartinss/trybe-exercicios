a0eda957573e2cc51ce4b4429cc07ddd7aa00270
germanoMartinss <germanopmartins91@gmail.com>
2023-05-31 22:10:20 -0300

    ExercicioDOIS

---
+++ _fundamentos/_secao-3-introducao-a-javascript/dia-1-primeiros-no-javascript/ExercícioDOIS.js
@@ -0,0 +1,11 @@
+const num1 = 140;
+const num2 = 165;
+
+
+if (num1 > num2) {
+    console.log(num1 + ' é maior que ' + num2);
+} else if (num1 === num2) {
+console.log('Os números são identicos');
+} else {
+    console.log(num2 + 'É maior que' + num1);
+}
