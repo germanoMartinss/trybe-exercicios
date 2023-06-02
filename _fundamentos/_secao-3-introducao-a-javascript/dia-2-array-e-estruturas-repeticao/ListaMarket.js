7f3506275472cffd283823eeae55f159c7a87c5a
germanoMartinss <germanopmartins91@gmail.com>
2023-06-01 21:31:31 -0300

    MarketList

---
+++ _fundamentos/_secao-3-introducao-a-javascript/dia-2-array-e-estruturas-repeticao/ListaMarket.js
@@ -0,0 +1,51 @@
+const laps = 68;
+
+for (index = 1; index <= laps; index += 1) {
+console.log(`Volta ${index} Completa!`);
+}
+console.log('Acabou a corrida!');
+
+//------------------------
+
+const market = ['Pão', 'Ovo', 'Carne', 'Tapioca', 'Brócolis', 'Cenoura', 'Banana', 'Mamão',];
+
+for (index = 0; index < market.length; index += 1) {
+    //console.log(index)
+    //console.log(market[index])
+    console.log(`${index} - ${market[index]}`);
+}
+//0 - Pão
+//1 - Ovo
+//2 - Carne
+//3 - Tapioca
+//4 - Brócolis
+//5 - Cenoura
+//6 - Banana
+//7 - Mamão
+
+//------------------------------
+
+const cars = ['Mercedes', 'Audi', 'Volvo', 'Honda', 'KIA', 'Hyndai'];
+
+for (let index = 0; index < cars.length; index += 1) {
+const currentCar = cars[index];
+console.log(currentCar);
+//console.log(`${index} - ${cars[index]}`);
+}
+
+//---------EXERCICIO---------
+
+const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
+
+for (index = 0; index < groceryList.length; index += 1) {
+  console.log(groceryList[index]);
+}
+
+//---- OU ---
+
+const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
+
+for (index = 0; index < groceryList.length; index += 1) {
+  const currentItem = groceryList[index];
+  console.log(currentItem);
+}
