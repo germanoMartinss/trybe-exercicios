9df77f8c212648819fc303f8f88fbb7fa2257797
germanoMartinss <germanopmartins91@gmail.com>
2023-07-10 11:53:08 -0300

    Para Fixar 1

---
+++ _fundamentos/_secao-7-higher-order-functions-do-JS/dia-1-intro-higher-order-functions/paraFixar1.js
@@ -0,0 +1,15 @@
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+  ];
+  
+  // Adicione seu código aqui
+  emailListInData.forEach((email) => {
+    console.log(`O email ${email} está cadastrado em nosso banco de dados!`);
+  });
+// O email roberta@email.com está cadastrado em nosso banco de dados!
+// O email paulo@email.com está cadastrado em nosso banco de dados!
+// O email anaroberta@email.com está cadastrado em nosso banco de dados!
+// O email fabiano@email.com está cadastrado em nosso banco de dados!
